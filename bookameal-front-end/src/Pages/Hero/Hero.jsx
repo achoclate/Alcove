218386147fbe926ba12682c2460ed3d0ccf423f1
achoclate <nyabokeann99@gmail.com>
2024-08-02 22:49:25 +0300

    create a reservation page for clients to book

---
+++ bookameal-front-end/src/Pages/Hero/Hero.jsx
@@ -0,0 +1,27 @@
+// src/components/Hero/Hero.js
+// import React from 'react';
+// import { Container, Row, Col } from 'react-bootstrap';
+// import heroImage from '../../assets/hero.jpeg'; // Correct path to the image
+// import './Hero.css'; // Custom CSS file for additional styling
+
+// const Hero = () => {
+//   return (
+//     <div className="hero-container" style={{ backgroundImage: `url(${heroImage})` }}>
+//       <Container className="hero-content">
+//         <Row className="justify-content-center text-center">
+//           <Col md={8}>
+//             <h1>Welcome to Slate Kitchen & Bar</h1>
+//             <p>Experience culinary excellence in the heart of Nairobi</p>
+//             <div className="hero-links">
+//               <a href="#reservation" className="hero-link">Reserve a Table</a>
+//               <span className="divider">|</span>
+//               <a href="#menu" className="hero-link">View the Menu</a>
+//             </div>
+//           </Col>
+//         </Row>
+//       </Container>
+//     </div>
+//   );
+// };
+
+// export default Hero;
