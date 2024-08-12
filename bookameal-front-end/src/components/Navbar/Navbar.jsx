b3a2d5126abae590bfd42a994a44c671dd9dbade
achoclate <nyabokeann99@gmail.com>
2024-08-12 15:19:04 +0300

    rectify css for all pages

--- bookameal-front-end/src/components/Navbar/Navbar.jsx
+++ bookameal-front-end/src/components/Navbar/Navbar.jsx
@@ -14,8 +14,8 @@ const Navigationbar = () => {
           <Nav className="navbar-nav">
             <Nav.Link as={Link} to="/">Home</Nav.Link>
             <Nav.Link as={Link} to="/about">About</Nav.Link>
-            <Nav.Link as={Link} to="#menu">Menu</Nav.Link>
-            <Nav.Link as={Link} to="#contact">Contact</Nav.Link>
+            <Nav.Link as={Link} to="/menu">Menu</Nav.Link>
+            <Nav.Link as={Link} to="/contact">Contact</Nav.Link>
             
             <NavDropdown title="Our Brands" id="basic-nav-dropdown" className="navbar-dropdown">
               <NavDropdown.Item as={Link} to="#brand1">Ocean's Bounty</NavDropdown.Item>
