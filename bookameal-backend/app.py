c9e76199c5998ce7f63f8a85e69f8cfc22d59165
achoclate <nyabokeann99@gmail.com>
2024-08-17 16:43:24 +0300

    update

--- bookameal-backend/app.py
+++ bookameal-backend/app.py
@@ -229,4 +229,5 @@ def delete_user(id):
         return jsonify({'message': 'User not found'}), 404
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    port = int(os.environ.get('PORT', 5000)) 
+    app.run(debug=True, host='0.0.0.0', port=port)
